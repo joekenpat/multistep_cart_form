9d48b2216cbb2167d7f1b779e9dc8e0b2cbc9c1d
Ibeandyson <ibeandyson123@gmail.com>
2021-02-19 17:11:21 +0100

    done mesh

--- js/app2.js
+++ js/app2.js
@@ -1,17 +1,16 @@
 $(document).ready(() => {
-  $("#build_layout_types").html(layoutTypeContent);
-  $("#edge_product_list").html(edgeProductList);
-  $("#build_layout_types")
-    .find("[data-layout-id]")
-    .each(function () {
-      $(this).on("click", function () {
-        selectedLayoutType = $(this).data("layout-id");
-      });
+    $('#build_layout_types').html(layoutTypeContent);
+    $('#edge_product_list').html(edgeProductList);
+    $('#build_layout_types').find('[data-layout-id]').each(function() {
+        $(this).on('click', function() {
+            selectedLayoutType = $(this).data('layout-id');
+        });
     });
 });
 
-const layoutTypeContent = layoutTypes.map((x) => {
-  return `<div class="col-4 px-1" data-layout-id="${x}">
+
+const layoutTypeContent = layoutTypes.map(x => {
+    return `<div class="col-4 px-1" data-layout-id="${x}">
   <div class="card" >
     <div class="card-body text-center">
       <p class="build-type-txt mb-0">${x}</p>
@@ -20,8 +19,8 @@ const layoutTypeContent = layoutTypes.map((x) => {
 </div>`;
 });
 
-const edgeProductList = edgeProducts.map((x) => {
-  return `<div class="col-4 px-1">
+const edgeProductList = edgeProducts.map(x => {
+    return `<div class="col-4 px-1">
   <div class="card" data-product-id="${x.id}">
     <div class="card-body p-0 text-center">
       <img
@@ -35,3 +34,4 @@ const edgeProductList = edgeProducts.map((x) => {
   </div>
 </div>`;
 });
+
